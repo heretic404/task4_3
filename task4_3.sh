afd4200e520501ad5cc96adf62f0f85c1aefbc5a
heretic404 <phoenix2160@gmail.com>
2018-03-29 16:31:36 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -35,14 +35,14 @@ fi
 #Archive settings
 srcdir="${1}"
 bnum="$2"
-bname="$(echo "$1" | sed -r 's/[/]+/-/g' | sed 's/^-//')"
+bname=$(echo "${1}" | sed -r 's/[/]+/-/g' | sed 's/^-//')
 filename=${bname}-$(date '+%Y-%m-%d-%H%M%S').tar.gz
 
 #Creating bacup file
 tar --create --gzip --file="$bdir$filename" "${srcdir}" 2> /dev/null
 
 #Checking bacup number and delete old ones
-rm -f $(find "$bdir" -name "${bname}*" -type f -printf "%Ts\t$bdir%P\n" | sort -n | head -n -"$2" | cut -f 2- )
+rm -f $(find "$bdir" -name "${bname}*" -type f -printf "%Ts\t$bdir"%P"\n" | sort -n | head -n -"$2" | cut -f 2- )
 
 
 echo "${bname}"
