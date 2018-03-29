9481ea78dec6cc4c24bb0d88b5ffc35725a3474f
heretic404 <phoenix2160@gmail.com>
2018-03-29 10:54:33 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 ARGS=2
 
+
+#Arguments check stage
+
+#Check number of arguments passed
+if [ "$#" -ne 1 ]; then
+    echo "error: Illegal number of parameters" >&2;
+    exit 1
+fi
 #Verifying given arguments
 if [ -z "$1" ] || [ -z "$2" ] 
   then
     echo "You need to specify a path to the folder which you want to backup and number of backups" >&2; 
     exit 1
 fi
-
 #Checing if first argument is a directory
 if ! [[ -d $1 ]]; then
     echo "error: $1 is a not a directory" >&2; 
     exit 2
 fi
-
 #Checing if second argument is a number
 re='^[0-9]+$'
 if ! [[ $2 =~ $re ]] ; then
    echo "error: $2 Not a number" >&2; 
    exit 2
 fi
+
+#
 
 echo "$1"
 echo "$2"
