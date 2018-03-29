86dfd90c11968980c8e119906ce438f99e381734
heretic404 <phoenix2160@gmail.com>
2018-03-29 11:16:12 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 ARGS=2
 
+#Backup settings
+bdir="/tmp/backups/"
+srcdir="$1"
+bnum="$2"
+bname=$(echo "$1" | sed -r 's/^.//[/]+/-/g')
+filename="$bname"-$(date +%-Y%-m%-d)-$(date +%-T).tar.gz
+
 
 #Arguments check stage
 
 #Check number of arguments passed
 if [ "$#" -ne 2 ]; then
     echo "error: Illegal number of parameters" >&2;
-    exit 1
-fi
-#Verifying given arguments
-if [ -z "$1" ] || [ -z "$2" ] 
-  then
     echo "You need to specify a path to the folder which you want to backup and number of backups" >&2; 
     exit 1
 fi
@@ -28,6 +30,12 @@ if ! [[ $2 =~ $re ]] ; then
 fi
 
 #
+if [ ! -d "$bdir" ]; then
+  mkdir "$bdir"
+fi
+tar --create --gzip --file=$bdir$filename $srcdir
+
 
 echo "$1"
 echo "$2"
+echo "$bdir"
