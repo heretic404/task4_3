4658cceab77de1efebc8eb2289f6ff24980772e6
heretic404 <phoenix2160@gmail.com>
2018-03-29 16:30:17 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -35,7 +35,7 @@ fi
 #Archive settings
 srcdir="${1}"
 bnum="$2"
-bname=$(echo "$1" | sed -r 's/[/]+/-/g' | sed 's/^-//')
+bname="$(echo "$1" | sed -r 's/[/]+/-/g' | sed 's/^-//')"
 filename=${bname}-$(date '+%Y-%m-%d-%H%M%S').tar.gz
 
 #Creating bacup file
