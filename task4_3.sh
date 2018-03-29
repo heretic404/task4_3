879771380cc8b23fd162bdd89ce4909670e326b4
heretic404 <phoenix2160@gmail.com>
2018-03-29 14:32:17 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -40,14 +40,16 @@ if [ ! -d "$bdir" ]; then
   mkdir "$bdir"
 fi
 
+#Creating bacup file
+tar --create --gzip --file=$bdir$filename $srcdir
+
 #Checking bacup number and delete old ones
 find "$bdir" -name "$bname*" -type f -printf '%Ts\t%P\n' | sort -n | head -n -"$2" | cut -f 2-| xargs rm -rf
 #rm $(find "$bdir" -name "$bname*" 2> /dev/null | sort | head -$(($bnum - $2 + 1 )) 2> /dev/null) 2> /dev/null
 rm -f $(find "$bdir" -name "$bname*" -type f -printf '%Ts\t%P\n' | sort -n | head -n -"$2" | cut -f 2- )#| xargs rm -rf
                             
 
-#Creating bacup file
-tar --create --gzip --file=$bdir$filename $srcdir
+
 
 
 
