b86e904aa49e756d82ceec7e4d4d46cbe74207c1
heretic404 <phoenix2160@gmail.com>
2018-03-29 15:11:44 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -45,7 +45,7 @@ tar --create --gzip --file=$bdir$filename $srcdir
 
 #Checking bacup number and delete old ones
 #find "$bdir" -name "$bname*" -type f -printf "%Ts\t%P\n" | sort -n | head -n -"$2" | cut -f 2-| xargs rm -f
-rm $(find "$bdir" -name "$bname*" -type f -printf "%Ts\t'$bdir'%P\n" | sort -n | head -n -"$2" | cut -f 2- )
+rm $(find "$bdir" -name "$bname*" -type f -printf "%Ts\t$bdir%P\n" | sort -n | head -n -"$2" | cut -f 2- )
                             
 
 
