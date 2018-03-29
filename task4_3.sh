16a7f16dcadfd934156537d1c98778dae2df0f9d
heretic404 <phoenix2160@gmail.com>
2018-03-29 10:47:53 +0300

    Update task4_3.sh

--- task4_3.sh
+++ task4_3.sh
@@ -4,8 +4,21 @@ ARGS=2
 #Verifying given arguments
 if [ -z "$1" ] || [ -z "$2" ] 
   then
-    echo "No argument supplied"
+    echo "You need to specify a path to the folder which you want to backup and number of backups" >&2; 
     exit 1
+fi
+
+#Checing if first argument is a directory
+if ! [[ -d $1 ]]; then
+    echo "error: $1 is a not a directory" >&2; 
+    exit 2
+fi
+
+#Checing if second argument is a number
+re='^[0-9]+$'
+if ! [[ $2 =~ $re ]] ; then
+   echo "error: $2 Not a number" >&2; 
+   exit 2
 fi
 
 echo "$1"
